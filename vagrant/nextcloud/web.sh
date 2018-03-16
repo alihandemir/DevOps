f53d788b87e387ea4a2f2b0b490db1375e940a1f
Alihan Demir <alihan.demir.6153@gmail.com>
2018-03-16 09:31:41 +0100

    Nextcloud Firewall

--- vagrant/nextcloud/web.sh
+++ vagrant/nextcloud/web.sh
@@ -14,8 +14,14 @@ sudo apt -y install unzip
 sudo unzip nextcloud-12.0.4.zip
 sudo mv ./nextcloud/* /var/www/html
 sudo chown -R www-data:www-data /var/www/html
-sudo apt-get install libapache2-mod-php7.0 php7.0-gd php7.0-json php7.0-mysql php7.0-curl
-sudo apt-get install php7.0-mbstring php7.0-intl php7.0-mcrypt php-imagick php7.0-xml php7.0-zip
+sudo apt -y install libapache2-mod-php7.0 php7.0-gd php7.0-json php7.0-mysql php7.0-curl
+sudo apt -y install php7.0-mbstring php7.0-intl php7.0-mcrypt php-imagick php7.0-xml php7.0-zip
+
+# Firewall regeln hinzufügen
+sudo apt -y install ufw
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw allow 8080/tcp
 
 # Maschine Neu starten
 sudo reboot
